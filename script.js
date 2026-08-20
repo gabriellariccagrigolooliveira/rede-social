994e5fb25aa52c1db53f52d6e3ef11d6c470bab5
gabriellariccagrigolooliveira <gabriella.grigolo.oliveira@escola.pr.gov.br>
2026-08-20 12:19:04 -0300

    ok

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ document.addEventListener("DOMContentLoaded",() => {
     if (likeBtn) return;
     const likeSvg = likeBtn.querySelector("svg");
 }
+    //localiza o contador
+
+    let textNode = Array.from(likeBtn.childNoodes).find(node) =>
+        node.nodeType
+    === Node.TEXT_NODE && node.textContent.trim() !== ""
+);
+
+//zera o contador
+let cont = 0;
+
+//atualiza
+if(textNode){
+    textNode.textContent = `0`;
+}
+
+
+
+//coração
+function applyLikedStyle (){
+    likeSvg.style.fill = "#ef4444";
+    likeSvg.style.stroke = "#ef4444";
+    likeSvg.style.color = "#ef4444";
+
+
+    //efeito curtida
+    likeSvg.style.transform = "scale(1.3";
+    setTimeout(() => (likeSvg.style.transform = "scale(1")150);
+}
